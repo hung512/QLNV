312b57e2c260c7de57038ac311470a86d87686bf
Phuong <HP>
2019-12-16 16:50:05 +0700

    C#

---
+++ QuanLyNhanVien/QuanLyNhanVien/RoomView.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuanLyNhanVien
+{
+    class RoomView
+    {
+        public int id { get; set; }
+        public string Name { get; set; }
+        public int Staff { get; set; }
+        public RoomView(ROOM room)
+        {
+            this.id = room.Id;
+            this.Name = room.NAME;
+            this.Staff = room.STAFFs.Count;
+        }
+    }
+}
