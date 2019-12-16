e2d2767e5e0efb9ef5104d9e9e1fc2885bdd3760
Phuong <HP>
2019-12-16 22:59:09 +0700

    C#

--- QuanLyNhanVien/QuanLyNhanVien/StatisticForm.Designer.cs
+++ QuanLyNhanVien/QuanLyNhanVien/StatisticForm.Designer.cs
@@ -29,46 +29,18 @@
         private void InitializeComponent()
         {
             this.grdStatistic = new System.Windows.Forms.DataGridView();
-            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
-            this.iDToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.rOOMToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             ((System.ComponentModel.ISupportInitialize)(this.grdStatistic)).BeginInit();
-            this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
             // grdStatistic
             // 
             this.grdStatistic.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.grdStatistic.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.grdStatistic.Location = new System.Drawing.Point(0, 28);
+            this.grdStatistic.Location = new System.Drawing.Point(0, 0);
             this.grdStatistic.Name = "grdStatistic";
             this.grdStatistic.RowTemplate.Height = 24;
-            this.grdStatistic.Size = new System.Drawing.Size(421, 311);
+            this.grdStatistic.Size = new System.Drawing.Size(421, 339);
             this.grdStatistic.TabIndex = 0;
-            // 
-            // menuStrip1
-            // 
-            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
-            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.iDToolStripMenuItem,
-            this.rOOMToolStripMenuItem});
-            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
-            this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(421, 28);
-            this.menuStrip1.TabIndex = 1;
-            this.menuStrip1.Text = "menuStrip1";
-            // 
-            // iDToolStripMenuItem
-            // 
-            this.iDToolStripMenuItem.Name = "iDToolStripMenuItem";
-            this.iDToolStripMenuItem.Size = new System.Drawing.Size(36, 24);
-            this.iDToolStripMenuItem.Text = "ID";
-            // 
-            // rOOMToolStripMenuItem
-            // 
-            this.rOOMToolStripMenuItem.Name = "rOOMToolStripMenuItem";
-            this.rOOMToolStripMenuItem.Size = new System.Drawing.Size(65, 24);
-            this.rOOMToolStripMenuItem.Text = "ROOM";
             // 
             // StatisticForm
             // 
@@ -76,23 +48,15 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(421, 339);
             this.Controls.Add(this.grdStatistic);
-            this.Controls.Add(this.menuStrip1);
-            this.MainMenuStrip = this.menuStrip1;
             this.Name = "StatisticForm";
             this.Text = "StatisticForm";
             ((System.ComponentModel.ISupportInitialize)(this.grdStatistic)).EndInit();
-            this.menuStrip1.ResumeLayout(false);
-            this.menuStrip1.PerformLayout();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.DataGridView grdStatistic;
-        private System.Windows.Forms.MenuStrip menuStrip1;
-        private System.Windows.Forms.ToolStripMenuItem iDToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem rOOMToolStripMenuItem;
     }
 }
