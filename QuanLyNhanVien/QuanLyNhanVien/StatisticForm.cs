a652154a575626c7fb2b0ef93e4d0407dc308268
Phuong <HP>
2019-12-16 16:58:15 +0700

    C#

--- QuanLyNhanVien/QuanLyNhanVien/StatisticForm.cs
+++ QuanLyNhanVien/QuanLyNhanVien/StatisticForm.cs
@@ -12,9 +12,28 @@ namespace QuanLyNhanVien
 {
     public partial class StatisticForm : Form
     {
+        private ClassManagement Business;
         public StatisticForm()
         {
             InitializeComponent();
+            this.Business = new ClassManagement();
+            this.Load += StatisticForm_Load;
+        }
+
+        void StatisticForm_Load(object sender, EventArgs e)
+        {
+            this.ShowRoom();
+        }
+        private void ShowRoom()
+        {
+            this.grdStatistic.DataSource = this.Business.GetRooms();
+            var room = this.Business.GetRooms();
+            var roomviews = new RoomView[room.Length];
+            for (int i = 0; i < roomviews.Length; i++)
+            {
+                roomviews[i] = new RoomView(room[i]);
+            }
+            this.grdStatistic.DataSource = roomviews;
         }
     }
 }
